836cdac187193c8d48122b2e2c5cee4e742b46b2
jcdavis3795 <46761218+jcdavis3795@users.noreply.github.com>
2020-10-31 14:25:33 -0500

    Update credentials_format.py

--- credentials_format.py
+++ credentials_format.py
@@ -5,3 +5,5 @@ ACCESS_KEY = ''
 ACCESS_SECRET = ''
 
 BEARER_TOKEN = ''
+
+#I've left the keys empty, but this is the format of the file that contains them. We don't use the bearer token in this project.
