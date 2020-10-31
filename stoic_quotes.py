550bddba75ec157babc6c716866e6ff4cb87558d
jcdavis3795 <46761218+jcdavis3795@users.noreply.github.com>
2020-10-31 14:22:44 -0500

    Update stoic_quotes.py

--- stoic_quotes.py
+++ stoic_quotes.py
@@ -1,16 +1,17 @@
 from bs4 import BeautifulSoup
 import requests
 
-
+# Information is passed into a few different lists as the script runs. I probably could have accomplished this with less lists, but this made it easier to code
+# and easier to read/understand what stage the information is in each list
 stoic_quotes = []
 stoic_quotes_decoded = []
 twit_quotes = []
 index = 0
 
-
+# This is the function to be used to visit urls and iterate through pages. The number of pages to iterate through is defined by web_page_num. 
+# the author is also added in this step. It was the least complicated way I could find to print the correct author at the end of their respective quotes
 def get_stoic_quotes(url, web_page_num, author):
-
-    # initialize list to contain the links to be scraped
+    
     global stoic_quotes_decoded
     global stoic_quotes
     stoic_links = []
@@ -36,11 +37,14 @@ def get_stoic_quotes(url, web_page_num, author):
                 quotes_raw = quote.find('div', class_='quoteText').text.strip().splitlines()
             except Exception as e:
                 quotes_raw = None
-
+                
+            # You never know what weird characters will show up while scraping for quotes. I needed to remove all non-ascii characters.
             stoic_quote = (quotes_raw[0]).encode('ascii', 'ignore')
 
             quote_decoded = stoic_quote.decode()
             stoic_quotes_decoded.append(quote_decoded)
+            
+            # list comprehension to get rid of quotes of some empty quotes
             stoic_quotes_decoded = [i for i in stoic_quotes_decoded if not i.startswith(' ')]
 
             full_quote = (stoic_quotes_decoded[inner_index], '\n', '\n', author)
@@ -49,14 +53,16 @@ def get_stoic_quotes(url, web_page_num, author):
 
 
 
-
+# Simply add the url to the goodreads page containing the stoic philosopher you want to scrape quotes for. I limited my own search to 5 pages as it seemed
+# after that, the quotes began repeating or were gernerally just incorrect. Since each page will contain 30 quotes, we end up with 450 quotes total
 get_stoic_quotes('https://www.goodreads.com/work/quotes/31010?', 5, 'Marcus Aurelius')
 get_stoic_quotes('https://www.goodreads.com/work/quotes/93900-epistulae-morales-ad-lucilium?', 5, 'Seneca The Younger')
 get_stoic_quotes('https://www.goodreads.com/work/quotes/1889585?', 5, 'Epictetus')
 
-
+# filter to get rid of quotes with a length greater than 281.
 stoic_quotes_clean = list(filter(lambda quote_text: len(quote_text) < 281, stoic_quotes))
 
+# since the list consists of tuples, I had to find another way to get all of the information I wanted in the correct format.
 for item in stoic_quotes_clean:
     twit_quotes.append(str(item[0]) + (item[1]) + (item[2]) + item[3])
     index += 1
